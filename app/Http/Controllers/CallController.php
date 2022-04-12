36fad6550298598643149c00be477ff01b668588
gnmg <ralph@ralphreene.info>
2022-04-13 00:30:50 +0900

    change regex

--- app/Http/Controllers/CallController.php
+++ app/Http/Controllers/CallController.php
@@ -65,7 +65,7 @@ class CallController extends Controller
         
         
         $rawNumber = ltrim($request->post('phone_numbers'),'0');
-        $phoneNo = '+81'.$rawNumber;
+        $phoneNo = '+'.$rawNumber;
 
         
         $call = $request->all();
@@ -142,8 +142,8 @@ class CallController extends Controller
             'title' => 'required|max:50',
             'time' => 'required',
             'timezone' => 'required',
-            'phone_numbers' => ['required', 'regex:/^0([0-9][0-9]{4}[0-9]{2}[0-9]{3}|[0-9]{3}[0-9]{2}|[0-9]{4}[0-9])[0-9]{4}$/'],
-            // 'phone_numbers' => 'required',
+            //'phone_numbers' => ['required', 'regex:/^0([0-9][0-9]{4}[0-9]{2}[0-9]{3}|[0-9]{3}[0-9]{2}|[0-9]{4}[0-9])[0-9]{4}$/'],
+            'phone_numbers' => 'required',
             'message_voice' => 'required',
             'message' => 'required',
             'sos' => 'required',
