403ae0249513a96e04168d55faab05056089be18
gnmg <ralph@ralphreene.info>
2022-04-12 19:10:52 +0900

    make migration

---
+++ database/migrations/2022_04_12_100141_add_ust_time_to_call_queue_column.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddUstTimeToCallQueueColumn extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('call_queue', function (Blueprint $table) {
+            $table->string('ust_time', 10)
+                    ->after('time');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('call_queue', function (Blueprint $table) {
+            $table->dropColumn('ust_time');
+        });
+    }
+}
