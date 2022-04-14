be2cf6e56271bb6f3a085b00beb4365a2aaeeef9
gnmg <ralph@ralphreene.info>
2022-04-14 22:29:09 +0900

    add chaged files

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -42,6 +42,10 @@
                                             style="text-align: start">
                                             {{ __('lang.time')}}
                                         </th>
+										 <th class="px-6 py-3 text-sm font-semibold leading-4 tracking-wider text-gray-700 uppercase border-b border-gray-200 bg-gray-50"
+                                            style="text-align: start">
+                                            {{ __('lang.timezone')}}
+                                        </th>
                                         <th class="px-6 py-3 text-sm font-semibold leading-4 tracking-wider text-gray-700 uppercase border-b border-gray-200 bg-gray-50"
                                             style="text-align: start">
                                             {{ __('lang.phone_number')}}
@@ -60,6 +64,10 @@
                                         <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
                                             <!-- {{str_replace(':00', '', $call -> time)}} -->
                                             {{$call -> time}}
+                                        </td>
+										<td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                            <!-- {{str_replace(':00', '', $call -> time)}} -->
+                                            {{$call -> timezone}}
                                         </td>
                                         <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
                                             0{{ $call -> phone_numbers }}
